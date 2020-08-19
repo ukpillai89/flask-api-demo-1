2c47a59735c6d0674aa8bf43d2e7fd45921ab3d6
Pillai <U6865@u-ssi.net>
2020-08-19 12:23:16 +0100

    First commit. added rest api to access store & items

---
+++ resources/item.py
@@ -0,0 +1,51 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help="This field cannot be blank")
+    parser.add_argument('store_id', type=int, required=True, help="This field cannot be blank")
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {"message": "Item not found"}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {"message": "An item with name '{}' already exists".format(name)}, 400
+
+        request_data = Item.parser.parse_args()
+        item = ItemModel(name, request_data['price'], request_data['store_id'])
+        try:
+            item.save_to_db()
+        except Exception as err:
+            return {"message": "An error occurred : {}".format(err)}, 500
+
+        return item.json(), 201
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+        return {"message": "Item was deleted"}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(name)
+        if item is None:
+            item = ItemModel(name, data['price'], data['store_id'])
+        else:
+            item.price = data['price']
+        item.save_to_db()
+
+        return item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': [item.json() for item in ItemModel.query.all()]}
